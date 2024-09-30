b072586b6cf5eb2fac2b51af37e80dd9a894f117
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2024-09-30 23:18:32 +0600

    Update update.sh

--- system-bash-files/update.sh
+++ system-bash-files/update.sh
@@ -1,15 +1,19 @@
 cd Desktop
 
-chmod +x Drive.sh
-
 chmod +x Shell.sh
+
+chmod +x Drive.sh
 
 chmod +x Stream.sh
 
-wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/Drive.sh
+chmod +x Goto.sh
 
 wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/Shell.sh
 
-wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/cmd.txt
+wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/Drive.sh
 
 wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/Stream.sh
+
+wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/Goto.sh
+
+wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/cmd.txt
