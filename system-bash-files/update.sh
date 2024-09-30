37457ceb1d5a08b152333259eebc647359c1555c
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2024-09-30 22:41:30 +0600

    Update update.sh

--- system-bash-files/update.sh
+++ system-bash-files/update.sh
@@ -7,3 +7,5 @@ chmod +x Shell.sh
 wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/Drive.sh
 
 wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/Shell.sh
+
+wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/cmd.txt
