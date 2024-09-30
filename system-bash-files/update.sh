7bb7d7317e7ab3c0937d753bb70e36eb5a2bff9c
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2024-09-30 22:36:18 +0600

    Update update.sh

--- system-bash-files/update.sh
+++ system-bash-files/update.sh
@@ -3,3 +3,7 @@ cd Desktop
 chmod +x Drive.sh
 
 chmod +x Shell.sh
+
+wget https://raw.githubusercontent.com/jahidrdpschool/pc/refs/heads/main/system-bash-files/Drive.sh
+
+wget https://raw.githubusercontent.com/jahidrdpschool/pc/refs/heads/main/system-bash-files/Shell.sh
