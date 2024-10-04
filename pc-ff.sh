31ab222b1c9f3763aff0ad6e0b020b2c94100886
Jahid Hasan <jahidnation@gmail.com>
2024-10-05 00:51:13 +0600

    Update pc-ff.sh

--- pc-ff.sh
+++ pc-ff.sh
@@ -5,7 +5,7 @@ download_and_run() {
   local port=$1
   local index=$2
 
-  docker run -p $port:80 -e DISPLAY=:0.0 -d stream_ff > /dev/null 2>&1
+  docker run -p $port:80 -e DISPLAY=:0.0 -d stream-ff > /dev/null 2>&1
   echo ""
   echo "Access Link $index: https://localhost:$port"
   echo ""
