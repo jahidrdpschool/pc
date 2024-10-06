808a7088996d7b1721b00befd844712da648cf6e
Jahid Hasan <jahidnation@gmail.com>
2024-10-06 22:55:57 +0600

    Update get.sh

--- system-bash-files/get.sh
+++ system-bash-files/get.sh
@@ -2,4 +2,6 @@ cd ~/Desktop
 
 wget https://github.com/jahidrdpschool/pc/raw/refs/heads/main/system-bash-files/update.sh
 
-chmod +x *.sh
+tr -d '\r' < update.sh > update_temp.sh && mv update_temp.sh update.sh
+
+chmod +x update.sh
