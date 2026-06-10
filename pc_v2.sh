93600a0ba2c5c92cbee7a61c4a9383193aecdf2c
Jahid Coder <jahid.coder.24@gmail.com>
2026-06-10 12:57:34 +0600

    Update pc_v2.sh

--- pc_v2.sh
+++ pc_v2.sh
@@ -5,7 +5,7 @@ download_and_run() {
   local port=$1
   local index=$2
 
-  docker run -p $port:3000 -e DISPLAY=:0.0 -d linuxserver/webtop > /dev/null 2>&1
+  docker run -p $port:3000 -e DISPLAY=:0.0 -d linuxserver/webtop:ubuntu-xfce > /dev/null 2>&1
   echo ""
   echo "Access Link $index: https://localhost:$port"
   echo ""
